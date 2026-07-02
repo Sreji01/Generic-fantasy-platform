2b0f95e3d72e4f7efe93fe81945c3e65df0109a2
Tamara Vucetic <tamara.vucetic@malkier.rs>
2026-07-02 14:53:23 +0200

    Initial commit - backend and frontend structure

---
+++ generic-fantasy-platform-backend/src/main/java/com/fantasy/platform/FantasyPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.fantasy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyPlatformApplication.class, args);
+    }
+}
